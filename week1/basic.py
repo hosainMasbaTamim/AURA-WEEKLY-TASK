d1c144222b4d8f6f7d1c8477efc2536f653631a8
hosainMasbaTamim <hosainmesba01715@gamil.com>
2025-11-02 12:29:08 +0600

    added loops and condtions

--- week1/basic.py
+++ week1/basic.py
@@ -13,3 +13,27 @@ z = {
     "a":2,
     "b":4
 }                       # dictionary
+
+print(a,b,c,d,x,y,z)
+
+# loops
+
+# for loop
+for i in range(5):      # looping through 0,1,2,3,4
+    print(i)
+
+# while loop
+while b < 10:           # loopging through 5,6,7,8,9
+    print(b)
+    b+=1
+
+# conditions
+b = 2*5
+c = 10
+if b > c :
+    print("b is  bigger")
+elif b < c :
+    print("c is bigger")
+else :
+    print("b c equal")
+
