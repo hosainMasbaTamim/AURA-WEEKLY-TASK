cf95eb928fb1544f861349e788eb1cfedd32e6e7
hosainMasbaTamim <hosainmesba01715@gamil.com>
2025-11-11 08:58:40 +0600

    added library module

--- week2/modules.py
+++ week2/modules.py
@@ -1,3 +1,10 @@
-import fn
+import fn                   # user defined module
+import math as mt           # pre-defined library module
 
-fn.oh_start_up()
+fn.oh_start_up()            # calling function from fn.py file
+
+print(mt.sqrt(16))          # calling function from library
+print(mt.pow(2, 3))    
+print(mt.factorial(5)) 
+print(mt.ceil(4.2))    
+print(mt.floor(4.8))   
